7a0af90e6a5aace4462a1d7798d6dfc2d16b34cd
dianat2021 <dianat.reza66@gmail.com>
2025-08-15 15:18:14 +0200

    implement try and catch in the register controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,8 +2,16 @@ import { User } from "../models/userModel.js";
 import { StatusCodes } from "http-status-codes";
 
 export const register = async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json(user);
+  try {
+    const user = await User.create(req.body);
+    res.status(StatusCodes.CREATED).json(user);
+  } catch (error) {
+    res.send(error);
+  }
+  // const { firstname, lastname, email, passwrod } = req.body;
+  // if (!firstname || !lastname || !email || !passwrod) {
+  //   res.status(StatusCodes.BAD_REQUEST).send("validation error");
+  // }
 };
 export const login = async (req, res) => {
   res.send("Login user...");
