e6842bc8414fb6f13c199592a0c06142990c222b
dianat2021 <dianat.reza66@gmail.com>
2025-08-16 18:10:23 +0200

    generate user token in register controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,19 @@
 import { User } from "../models/userModel.js";
 import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     // const { firstname, lastname, email, passwrod } = req.body;
     // if (!firstname || !lastname || !email || !passwrod) {
     //   res.status(StatusCodes.BAD_REQUEST).send("validation error");
     // }
-
     const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json(user);
+    const token = jwt.sign(
+      { userID: user._id, firstname: user.firstname },
+      "jwtSecret",
+      { expiresIn: "30d" }
+    );
+    res.status(StatusCodes.CREATED).json({ user: user.firstname, token });
   } catch (error) {
     res.send(error);
   }
