ed327ed9124abe1dcbf71342deeebb81ac0654c8
dianat2021 <dianat.reza66@gmail.com>
2025-08-18 15:55:31 +0200

    create the auth middleware

---
+++ backend/middleware/authentication.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel";
+import { StatusCodes } from "http-status-codes";
+
+export const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    res.status(StatusCodes.UNAUTHORIZED).send("Invalid authentication");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    // Attaching the authenticated user to the request.body
+    req.user = {
+      userID: payload.userID,
+      firstname: payload.firstname,
+      lastname: payload.lastname,
+    };
+    next();
+  } catch (error) {
+    res.status(StatusCodes.UNAUTHORIZED).send("Invalid authentication");
+  }
+};
