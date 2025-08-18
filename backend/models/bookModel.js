2613dfbfe39a0b3422ce56220c12c488be6a8415
dianat2021 <dianat.reza66@gmail.com>
2025-08-18 16:39:44 +0200

    create book schema

---
+++ backend/models/bookModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    bookTitle: {
+      type: String,
+      required: [true, "Book title is required"],
+      minLength: 1,
+      maxLength: 100,
+      trim: true,
+    },
+    bookAuthor: {
+      type: String,
+      required: [true, "Book author is required"],
+      minLength: 1,
+      maxLength: 100,
+      trim: true,
+    },
+    startingDate: {
+      type: Date,
+      required: [true, "Starting date is required"],
+    },
+    readingStatus: {
+      type: String,
+      enum: {
+        values: ["ongoing", "finished", "stopped"],
+        message: `{VALUE} is not supported`,
+      },
+      required: [true, "Reading status is required"],
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Book = mongoose.model("Book", bookSchema);
