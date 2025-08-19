75df2c7e743d0eb2afd4eea74d9435d0dcc50903
dianat2021 <dianat.reza66@gmail.com>
2025-08-19 12:23:49 +0200

    implement the code in the controller to create a book

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -1,3 +1,6 @@
+import { StatusCodes } from "http-status-codes";
+import { Book } from "../models/bookModel.js";
+
 export const getAllBooks = async (req, res) => {
   res.send("Get all books");
 };
@@ -5,7 +8,17 @@ export const getBook = async (req, res) => {
   res.send("Get a book");
 };
 export const createBook = async (req, res) => {
-  res.json(req.user);
+  try {
+    req.body.createdBy = req.user.userID;
+    const book = await Book.create(req.body);
+    res.status(StatusCodes.CREATED).json(book);
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({
+        error: error.message || "Something went wrong while creating the book.",
+      });
+  }
 };
 export const deleteBook = async (req, res) => {
   res.send("Delete a book");
